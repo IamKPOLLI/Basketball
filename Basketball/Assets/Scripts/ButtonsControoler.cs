91dadb518b61720adfdb91719ce1cbcdc0b3e51d
IamKPOLLI <aleksdavydoveu@gmail.com>
2020-12-07 20:57:29 +0300

    Full Project

---
+++ Basketball/Assets/Scripts/ButtonsControoler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsControoler : MonoBehaviour
+{
+
+
+    [SerializeField] private AudioSource _ButtonClickAudion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void LoadPlayScene()
+    {
+        _ButtonClickAudion.Play();
+        SceneManager.LoadScene("GameProcess");
+    }
+
+    public void LoadMenu()
+    {
+        _ButtonClickAudion.Play();
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Basketball/Assets/Scripts/RingController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class RingController : MonoBehaviour
+{
+    
+    [SerializeField] private AudioSource _plusScoreAudio;
+    [SerializeField] private AudioSource _gameOverAudio;
+    [SerializeField] private GameController _gameController; 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            _gameOverAudio.Play();
+            Destroy(collision.gameObject);
+            _gameController.currentGameState = GameController.GameState.end;
+        }   
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            _plusScoreAudio.Play();
+            Destroy(collision.gameObject);
+            _gameController.SumScore();
+        }
+    }
+
+
+}
